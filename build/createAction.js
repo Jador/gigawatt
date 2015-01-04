adfd51a8c91f9bb64df131c9570cf309afd75cc4
Tyler Graham <tyler.graham.prog@gmail.com>
2015-01-04 17:43:22 -0600

    Modified create action so as to be able to dig up the name and to return just the action if the argument isn't an array

--- build/createAction.js
+++ build/createAction.js
@@ -1,10 +1,13 @@
 "use strict";
 var Bus = require('baconjs').Bus;
-function Action() {
+function Action(name) {
   var bus = new Bus();
   var functor = function() {
     functor.emit.apply(functor, arguments);
   };
+  functor.getName = (function() {
+    return name;
+  });
   functor.listen = (function() {
     return bus;
   });
@@ -15,10 +18,14 @@ function Action() {
 }
 var createAction = function(actionName) {
   var obj = {};
-  [].concat(actionName).forEach((function(name) {
-    return obj[name] = new Action();
+  [].concat(actionName).forEach((function(action) {
+    return obj[action] = new Action(action);
   }));
-  return obj;
+  if (Object.keys(obj).length === 1) {
+    return obj[actionName];
+  } else {
+    return obj;
+  }
 };
 module.exports = createAction;
 //# sourceURL=createAction.js
--- test/createAction.test.js
+++ test/createAction.test.js
@@ -1,21 +1,25 @@
 var Gigawatt = require('../');
 
-describe('test of createAction', function() {
+describe('createAction', function() {
 
-  it('should create object with property "bob"', function() {
-    var x = Gigawatt.createAction('bob');
-    expect(x.hasOwnProperty('bob')).toBe(true);
+  it('should create a single action when given a string', function() {
+    expect(typeof Gigawatt.createAction('bob')).toBe('function');
   });
 
-  it('should be a function', function() {
-    expect(typeof Gigawatt.createAction('bob').bob).toBe('function');
+  it('should have the name provided to the factory method', function() {
+    expect(Gigawatt.createAction('bob').getName()).toBe('bob');
   });
 
-  it('should create multiple properties when given an array', function() {
+  it('should create an object when given an array', function() {
     var actions = ['alpha', 'omega', 'gamma'];
-    var x = Gigawatt.createAction(actions);
-    actions.forEach(function(action) {
-      expect(x.hasOwnProperty(action)).toBe(true);
+    expect(typeof Gigawatt.createAction(actions)).toBe('object');
+  });
+
+  it('should have actions(properties) for each string in the array', function() {
+    var actionNames = ['alpha', 'omega', 'gamma'];
+    var actions = Gigawatt.createAction(actionNames);
+    actionNames.forEach(function(name) {
+      expect(actions.hasOwnProperty(name)).toBe(true);
     });
   });
   
@@ -24,13 +28,13 @@ describe('test of createAction', function() {
     var i = 0;
 
     var int = setInterval(function() {
-      x.bob(i++);
+      x(i++);
       if(i > 3) {
         stop();
       }
     }, 500);
 
-    x.bob.listen().onValue(function(value) {
+    x.listen().onValue(function(value) {
       expect(value).not.toBeNull();
       console.log("listen value: ", value);
     });
